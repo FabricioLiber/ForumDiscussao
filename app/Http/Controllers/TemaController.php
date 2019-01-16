48e6b244c2bee57fde55a14d1484db0e8ce35bf4
Fabrício <fabricio.liber@gmail.com>
2019-01-16 14:23:51 -0300

    Adicionada views e metodos no controller de Tema

---
+++ app/Http/Controllers/TemaController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use \App\Tema;
+
+class TemaController extends Controller
+{
+    //
+    public function index () {
+        return view('temas')->with('temas', \App\Tema::all());
+    }
+
+    public function getViewCadastrar () {
+        return view('cadastrarTema');
+    }
+
+    public function cadastrar (Request $request) {
+
+        $tema = new Tema();
+        $tema->nome = $request->input('nome');
+        $tema->descricao = $request->input('descricao');
+        $tema->save();
+        return redirect('/temas');
+    }
+
+    public function getViewAtualizar ($id) {
+        $tema = Tema::find($id);
+        return view('atualizar_tema', $tema);
+    }
+
+    public function atualizar () {
+
+    }
+
+    public function deletar () {
+
+    }
+}

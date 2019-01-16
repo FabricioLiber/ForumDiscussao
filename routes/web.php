2dd86348e384865a2988be60671a620ce65e2f7a
Fabrício <fabricio.liber@gmail.com>
2019-01-16 14:20:53 -0300

    gerada as rotas de Tema

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,21 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('temas/', 'TemaController@index');
+
+//, 'middleware' =>'auth'
+
+Route:: group(['prefix' => 'temas', 'middleware' =>'auth'], function(){
+//  Route::get('/', 'TemaController@index');
+    Route::get('cadastrar', 'TemaController@getViewCadastrar');
+	Route::post('cadastrar', 'TemaController@cadastrar');
+    Route::get('atualizar/{id}', 'TemaController@getViewAtualizar');
+    Route::put('atualizar', 'TemaController@atualizar');
+    Route::patch('atualizar', 'TemaController@realizarAtualizacaoParcial');
+    Route::delete('/{id}', 'TemaController@deletar');
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');

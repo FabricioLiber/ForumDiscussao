04613ba3632fb617b07ff8311ab8f9f16605fc89
Fabrício <fabricio.liber@gmail.com>
2019-02-20 14:28:36 -0300

    Alteracoes na font e paginacao

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     @yield('styles')
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
         integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Bree+Serif|Black+Ops+One" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Black+Ops+One" rel="stylesheet">
 </head>
 <body>
     <div id="app">

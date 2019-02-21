444118a740c3e2b825f749fb7e42e6d23b81bd5c
Kamila <kmilaporto1991@gmail.com>
2019-02-21 15:33:26 -0300

    ajusta tela list postagens

--- resources/views/postagem/list.blade.php
+++ resources/views/postagem/list.blade.php
@@ -8,10 +8,13 @@
 
 @section('content')
     <div class="container">
-        <h1 class="title">Postagens</h1>        
-        @auth
-        <a href="{{url('postagens/cadastrar')}}" id="addPostagem"><i class="fas fa-plus-circle"></i></a>
-        @endauth
+        <div style="display:flex; justify-content: space-between;  ">
+            <h1 class="title">Postagens</h1>
+            @auth
+            <a href="{{url('postagens/cadastrar')}}" id="addPostagem"><i class="fas fa-plus-circle"></i></a>
+            @endauth
+        </div>
+        <hr>
         @foreach ($postagens as $postagem)
             <div class="blog-post">
                 <h2 class="blog-post-title">{{$postagem->titulo}}</h2>
@@ -28,32 +31,12 @@
                         </form>
                     </div>
                 @endif
-                <h3>Respostas</h3>
-                @forelse ($postagem->respostas as $resposta)
-                    <p>{{$resposta->descricao}}</p>
-                    <p class="blog-post-meta ">{{date("d M Y", $resposta->creat_at)}}, by {{$resposta->user->name}}</p>
-                    @if ($resposta->user == Auth::user())
-                    <div class="links-table">
-                        <a href="{{url('respostas/'.$postagem->id.'/atualizar/'.$resposta->id)}}" method="put" class="icon-edit"><i class="fas fa-edit"></i></a>
-                        <form action="{{url('respostas/'.$postagem->id.'/deletar/'.$resposta->id)}}" method="post" style="display: inline;">
-                            {{csrf_field()}}
-                            {{ method_field('delete') }}
-                            <button type="submit" id="icon-submit" class="icon-trash"><i class="fas fa-trash-alt"></i></button>
-                        </form>
-                    </div>
-                    @endif
-                @empty 
-                @endforelse
-                @auth
-                    <form method="post" action="{{ url('/respostas/'.$postagem->id.'/cadastrar') }}">
-                        {{csrf_field()}}
-                        <div class="form-group">
-                            <input type="text" class="form-control" name="resposta" id="respostaInput">
-                            <input type="submit" rows="4" class="btn btn-primary" value="cadastrar">
-                        </div>
-                    </form>    
-                @endauth
+                <div class="text-right" >
+                    <a  href="{{url('postagem/'.$postagem->id)}}">ver mais..</a>
+                </div>
+                <hr>
             </div>
+
         @endforeach
         {{$postagens->render()}}
     </div>
